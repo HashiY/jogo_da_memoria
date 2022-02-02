1db566fdc5366fec87e02302127a21a7a0b887d1
VSF <47865897+HashiY@users.noreply.github.com>
2022-02-02 14:40:31 -0300

    Parte 5

--- script.js
+++ script.js
@@ -1,8 +1,47 @@
 const cards = document.querySelectorAll('.card');
+let hasFlippedCard = false;
+let firstCard, secondCard;
+//Bloqueia o tabuleiro para nao ocorrer o bug de conseguir clicar em mais que duas cartas
+let lockBoard = false;
 
-//função para virar carta
+//função para virar a carta
 function flipCard() {
-    this.classList.toggle('flip');
+    if(lockBoard) return;
+
+    this.classList.add('flip');
+    if(!hasFlippedCard){
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+    hasFlippedCard = false;
+    checkForMatch();
+}
+
+//função que desabilita as cartas
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+}
+
+//funcão que desvira as cartas
+function unflipCards() {
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+    }, 1500);
+}
+
+//função que checa se as cartas são iguais
+function checkForMatch(){
+    //Compara as data-card que criou no html
+    if(firstCard.dataset.card === secondCard.dataset.card) {
+        disableCards();
+        return;
+    }
+    unflipCards();
 }
 
 //adiciona evento de clique para cada carta
